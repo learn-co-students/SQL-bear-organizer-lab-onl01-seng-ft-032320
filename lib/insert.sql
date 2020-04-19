247bd247669b3373c3b0924856e7dc56561e5c38
antdp425 <antdp425@aol.com>
2020-04-18 20:02:56 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name,age,gender,color,temperament,alive) 
+   VALUES ("Mr. Chocolate",2,"Male","Brown","Angry",0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) 
+   VALUES ("Rowdy",2,"Male","Brown","Angry",0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) 
+   VALUES ("Tabitha",2,"Male","Brown","Angry",0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) 
+   VALUES ("Sergeant Brown",2,"Male","Brown","Angry",0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) 
+   VALUES ("Melissa",2,"Male","Brown","Angry",0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) 
+   VALUES ("Grinch",2,"Male","Brown","Angry",0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) 
+   VALUES ("Wendy",2,"Male","Brown","Angry",0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) 
+   VALUES (NULL,NULL,NULL,NULL,NULL,NULL);
