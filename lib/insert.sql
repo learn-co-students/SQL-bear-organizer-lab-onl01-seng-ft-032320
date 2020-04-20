47a4db7b9d2b018e741f420edad32cd15a6f7741
BenWix <benwichser@gmail.com>
2020-04-20 10:50:28 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 10, "Male", "Chocolate", "Doscile", 1),
+(2,"Rowdy", "Female", 5, "Brown", "Rowdy", 1),
+(3,"Tabitha", "Female", 18, "Black", "Lethargic", 0),
+(4,"Sergeant Brown", 3,"Male", "Gray", "Aggressive", 1),
+(5,"Melissa", 15,"Female", "Black", "Loving", 0),
+(6,"Grinch", 12,"Male", "White", "Festive", 1),
+(7,"Wendy", 8, "Female", "Chocolate", "Doscile", 1),
+(8, null, 20, "Male", "Brown", "Hungry", 0);
+
+--INSERT INTO bears (id, age, gender, color, temperament, alive) VALUES 
